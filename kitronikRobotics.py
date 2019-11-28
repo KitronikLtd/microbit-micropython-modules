6456912bfe810e3f51c908a8950a9cb69e3c7e11
AlasdairAtKitronik <alasdair.trench@kitronik.co.uk>
2019-11-28 11:58:34 +0000

    Added Robotics board module

---
+++ kitronikRobotics.py
@@ -0,0 +1,188 @@
+# microbit-module: kitronikRobotics@0.0.1
+from microbit import *
+
+class settings:
+    PRESCALE_REG = 0xFE
+    MODE_1_REG = 0x00
+    SRV_REG_BASE = 0x08
+    MOT_REG_BASE = 0x28
+    REG_OFFSET = 4
+    SERVO_MULTIPLIER = 226
+    SERVO_ZERO_OFFSET = 0x66
+
+    chipAddress = 0x6C
+    initialised = False
+    stepStage = 1
+    stepper1Steps = 200
+    stepper2Steps = 200
+
+def __init():
+        
+    buf = bytearray(2)
+
+    buf[0] = settings.PRESCALE_REG
+    buf[1] = 0x85 #50Hz
+    i2c.write(settings.chipAddress, buf, False)
+    
+    for blockReg in range(0xFA, 0xFE, 1):
+        buf[0] = blockReg
+        buf[1] = 0x00
+        i2c.write(settings.chipAddress, buf, False)
+
+    buf[0] = settings.MODE_1_REG
+    buf[1] = 0x01
+    i2c.write(settings.chipAddress, buf, False)
+    settings.initialised = True
+
+def servoWrite(servo, degrees):
+    if settings.initialised is False:
+        __init()
+    buf = bytearray(2)
+    calcServo = settings.SRV_REG_BASE + ((servo - 1) * settings.REG_OFFSET)
+    HighByte = False
+    PWMVal = (degrees * 100 * settings.SERVO_MULTIPLIER) / (10000 + settings.SERVO_ZERO_OFFSET)
+    
+    if (PWMVal > 0xFF):
+        HighByte = True
+    buf[0] = calcServo
+    buf[1] = int(PWMVal)
+    i2c.write(settings.chipAddress, buf, False)
+    buf[0] = calcServo + 1
+    if (HighByte):
+        buf[1] = 0x01
+    else:
+        buf[1] = 0x00
+    i2c.write(settings.chipAddress, buf, False)
+
+def motorOn(motor, direction, speed):
+    if settings.initialised is False:
+        __init()
+    buf = bytearray(2)
+    motorReg = settings.MOT_REG_BASE + (2 * (motor - 1) * settings.REG_OFFSET)
+    HighByte = False
+    OutputVal = speed * 40
+    
+    if direction == "forward":
+        if OutputVal > 0xFF:
+            HighByte = True
+            HighOutputVal = int(OutputVal/256)
+        buf[0] = motorReg
+        buf[1] = int(OutputVal)
+        i2c.write(settings.chipAddress, buf, False)
+        buf[0] = motorReg + 1
+        if HighByte:
+            buf[1] = HighOutputVal
+        else:
+            buf[1] = 0x00
+        i2c.write(settings.chipAddress, buf, False)
+        
+        for offset in range(4, 6, 1):
+            buf[0] = motorReg + offset
+            buf[1] = 0x00
+            i2c.write(settings.chipAddress, buf, False)
+        
+    elif direction == "reverse":
+        if OutputVal > 0xFF:
+            HighByte = True
+            HighOutputVal = int(OutputVal/256)
+        buf[0] = motorReg + 4
+        buf[1] = int(OutputVal)
+        i2c.write(settings.chipAddress, buf, False)
+        buf[0] = motorReg + 5
+        if HighByte:
+            buf[1] = HighOutputVal
+        else:
+            buf[1] = 0x00
+        i2c.write(settings.chipAddress, buf, False)
+        
+        for offset2 in range(0, 2, 1):
+            buf[0] = motorReg + offset2
+            buf[1] = 0x00
+            i2c.write(settings.chipAddress, buf, False)
+
+def motorOff(motor):
+    buf = bytearray(2)
+    motorReg = settings.MOT_REG_BASE + (2 * (motor - 1) * settings.REG_OFFSET)
+    
+    for offset3 in range(0, 2, 1):
+        buf[0] = motorReg + offset3
+        buf[1] = 0x00
+        i2c.write(settings.chipAddress, buf, False)
+    
+    for offset4 in range(4, 6, 1):
+        buf[0] = motorReg + offset4
+        buf[1] = 0x00
+        i2c.write(settings.chipAddress, buf, False)
+
+def allOff():
+    buf = bytearray(2)
+    servoOffCount = 0
+    servoRegCount = 0
+    
+    for motors in range(1, 5, 1):
+        motorOff(motors)
+
+    while servoOffCount < 8:
+        for offset5 in range(0, 2, 1):
+            buf[0] = settings.SRV_REG_BASE + servoRegCount + offset5
+            buf[1] = 0x00
+            i2c.write(settings.chipAddress, buf, False)
+
+        servoRegCount += 4
+        servoOffCount += 1
+
+def stepperMotorTurnAngle(stepper, direction, angle):
+    angleToSteps = 0
+
+    if settings.initialised is False: 
+        __init()
+
+    if stepper == "Stepper1":
+        angleToSteps = ((angle - 1) * (settings.stepper1Steps - 1)) / (360 - 1) + 1
+    else:
+        angleToSteps = ((angle - 1) * (settings.stepper2Steps - 1)) / (360 - 1) + 1
+
+    angleToSteps = int(angleToSteps)
+    _turnStepperMotor(stepper, direction, angleToSteps)
+
+def stepperMotorTurnSteps(stepper, direction, stepperSteps):
+    if settings.initialised is False: 
+        __init()
+
+    _turnStepperMotor(stepper, direction, stepperSteps)
+
+def _turnStepperMotor(stepper, direction, steps):
+    stepCounter = 0
+
+    while stepCounter < steps:
+        if settings.stepStage == 1 or settings.stepStage == 3:
+            if stepper == "Stepper1":
+                currentMotor = 1
+            else:
+                currentMotor = 3
+        else:
+            if stepper == "Stepper1":
+                currentMotor = 2
+            else:
+                currentMotor = 4
+
+        if settings.stepStage == 1 or settings.stepStage == 4:
+             currentDirection = "forward"
+        else:
+            currentDirection = "reverse"
+
+        motorOn(currentMotor, currentDirection, 100)
+        sleep(20)
+
+        if direction == "forward":
+            if settings.stepStage == 4: 
+                settings.stepStage = 1
+            else:
+                settings.stepStage += 1
+        elif direction == "reverse":
+            if settings.stepStage == 1: 
+                settings.stepStage = 4
+            else:
+                settings.stepStage -= 1
+        
+        stepCounter += 1
