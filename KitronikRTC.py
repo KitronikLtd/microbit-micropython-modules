2095e14d28124c761209ae19869e8832c437c8bc
PhilAtKitronik <phil.bradwell@kitronik.co.uk>
2019-12-02 09:52:55 +0000

    Read and set Time

--- KitronikRTC.py
+++ KitronikRTC.py
@@ -1,32 +1,33 @@
 # microbit-module: kitronikRTC@0.0.1
 # A module for the Kitronik RTC chip
-from microbit import *
+from microbit import i2c
+from microbit import pin19
+from microbit import pin20
 
 class RTC:
-    initalised = False
-    CHIP_ADDRESS = 0x6F 	
-    RTC_SECONDS_REG = 0x00		
-    RTC_MINUTES_REG = 0x01		
-    RTC_HOURS_REG = 0x02			
-    RTC_WEEKDAY_REG = 0x03		
-    RTC_DAY_REG = 0x04			
-    RTC_MONTH_REG = 0x05			
-    RTC_YEAR_REG = 0x06			
-    RTC_CONTROL_REG = 0x07		
-    RTC_OSCILLATOR_REG = 0x08 	
-    RTC_PWR_UP_MINUTE_REG = 0x1C  
+    CHIP_ADDRESS = 0x6F
+    RTC_SECONDS_REG = 0x00
+    RTC_MINUTES_REG = 0x01
+    RTC_HOURS_REG = 0x02
+    RTC_WEEKDAY_REG = 0x03
+    RTC_DAY_REG = 0x04
+    RTC_MONTH_REG = 0x05
+    RTC_YEAR_REG = 0x06
+    RTC_CONTROL_REG = 0x07
+    RTC_OSCILLATOR_REG = 0x08
+    RTC_PWR_UP_MINUTE_REG = 0x1C
     START_RTC = 0x80
     STOP_RTC = 0x00
     ENABLE_BATTERY_BACKUP = 0x08
-    currentSeconds = 0			
+    currentSeconds = 0
     currentMinutes = 0
     currentHours = 0
     currentWeekDay = 0
     currentDay = 0
     currentMonth = 0
-    currentYear = 0  		
-    
-    def init(self):
+    currentYear = 0
+
+    def __init__(self):
         i2c.init(freq=100000, sda=pin20, scl=pin19)
         writeBuf = bytearray(2)
         readBuf = bytearray(1)
@@ -49,25 +50,20 @@ class RTC:
         writeBuf[0] = self.RTC_SECONDS_REG
         writeBuf[1] = self.START_RTC | readCurrentSeconds
         i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-        self.initalised = True
 
     def decToBcd(self, decNumber):
         tens = 0
         units = 0
         bcdNumber = 0
-        tens = decNumber / 10
+        tens = decNumber // 10
         units = decNumber % 10
         bcdNumber = (tens << 4) | units
         return bcdNumber
 
     def bcdToDec(self, bcdNumber, readReg):
         mask = 0
-        shiftedTens = 0
-        units = 0
-        tens = 0
-        decNumber = 0
-        if readReg == self.RTC_SECONDS_REG:
-            mask = 0x30
+        if readReg is self.RTC_SECONDS_REG:
+            mask = 0x70
         elif readReg is self.RTC_MINUTES_REG:
             mask = 0x70
         elif readReg is self.RTC_DAY_REG:
@@ -82,11 +78,10 @@ class RTC:
         tens = bcdNumber & mask
         shiftedTens = tens >> 4
         decNumber = (shiftedTens * 10) + units
+        print(decNumber)
         return decNumber
-        
+
     def readValue(self): 
-        if self.initalised is False:
-            self.init(self)
         writeBuf = bytearray(1)
         readBuf = bytearray(7)
         self.readCurrentSeconds = 0
@@ -100,13 +95,22 @@ class RTC:
         self.currentDay = readBuf[4]
         self.currentMonth = readBuf[5]
         self.currentYear = readBuf[6]
-        
+
+    def readTime(self, readRequest): 
+        timeRead=0
+        self.readValue()
+        if readRequest is "hours":
+            timeRead = self.bcdToDec(self.currentHours, self.RTC_HOURS_REG)
+        elif readRequest is "minutes":
+            timeRead = self.bcdToDec(self.currentMinutes, self.RTC_MINUTES_REG)
+        elif readRequest is "seconds":
+            timeRead = self.bcdToDec(self.currentSeconds, self.RTC_SECONDS_REG)
+        return timeRead
+
     def setTime(self, setHours, setMinutes, setSeconds): 
-        if self.initalised is False:
-            self.init(self)	
-        bcdHours = self.decToBcd(self, setHours)
-        bcdMinutes = self.decToBcd(self, setMinutes)
-        bcdSeconds = self.decToBcd(self, setSeconds)
+        bcdHours = self.decToBcd(setHours)
+        bcdMinutes = self.decToBcd(setMinutes)
+        bcdSeconds = self.decToBcd(setSeconds)
         writeBuf = bytearray(2)
         writeBuf[0] = self.RTC_SECONDS_REG
         writeBuf[1] = self.STOP_RTC
@@ -119,59 +123,4 @@ class RTC:
         i2c.write(self.CHIP_ADDRESS, writeBuf, False)
         writeBuf[0] = self.RTC_SECONDS_REG
         writeBuf[1] = self.START_RTC | bcdSeconds
-        i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-
-    def readTimeAndDate(self): 
-        if self.initalised is False:
-            self.init(self)
-        self.readValue(self)
-        decSeconds = str(self.bcdToDec(self, self.currentSeconds, self.RTC_SECONDS_REG))
-        decMinutes = str(self.bcdToDec(self, self.currentMinutes, self.RTC_MINUTES_REG))
-        decHours = str(self.bcdToDec(self, self.currentHours, self.RTC_HOURS_REG))
-        decDay = str(self.bcdToDec(self, self.currentDay, self.RTC_DAY_REG))
-        decMonths = str(self.bcdToDec(self, self.currentMonth, self.RTC_MONTH_REG))
-        decYears = str(self.bcdToDec(self, self.currentYear, self.RTC_YEAR_REG))
-        timeAndDate = "" + (decHours) + ":" + (decMinutes) + ":" + (decSeconds) + "   " + (decDay) + "/" + (decMonths) + "/" + (decYears)
-        return timeAndDate
-
-    def setDate(self, setDay, setMonth, setYear): 
-        if self.initalised is False:
-            self.init(self)
-        leapYearCheck = 0
-        writeBuf = bytearray(2)
-        readBuf = bytearray(1)
-        bcdDay = 0
-        bcdMonths = 0
-        bcdYears = 0
-        readCurrentSeconds = 0
-        if setMonth is 4 or 6 or 9 or 11:
-            if setDay is 30:
-                setDay = 30
-        if setMonth is 2 and setDay is 29:
-            leapYearCheck = setYear % 4
-            if leapYearCheck is 0:
-                setDay = 29
-            else:
-                setDay = 28
-        bcdDay = self.decToBcd(self, setDay)
-        bcdMonths = self.decToBcd(self, setMonth)
-        bcdYears = self.decToBcd(self, setYear)
-        writeBuf[0] = self.RTC_SECONDS_REG
-        i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-        readBuf = i2c.read(self.CHIP_ADDRESS, 1, False)
-        readCurrentSeconds = readBuf[0]
-        writeBuf[0] = self.RTC_SECONDS_REG
-        writeBuf[1] = self.STOP_RTC
-        i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-        writeBuf[0] = self.RTC_DAY_REG
-        writeBuf[1] = bcdDay
-        i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-        writeBuf[0] = self.RTC_MONTH_REG
-        writeBuf[1] = bcdMonths
-        i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-        writeBuf[0] = self.RTC_YEAR_REG
-        writeBuf[1] = bcdYears
-        i2c.write(self.CHIP_ADDRESS, writeBuf, False)
-        writeBuf[0] = self.RTC_SECONDS_REG
-        writeBuf[1] = self.START_RTC | readCurrentSeconds
         i2c.write(self.CHIP_ADDRESS, writeBuf, False)
