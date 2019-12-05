daa583b5b0e184c24fba26901716b96a3cb589fd
AlasdairAtKitronik <alasdair.trench@kitronik.co.uk>
2019-12-05 13:37:31 +0000

    Added some range functionality, needs more work

--- KitronikZip.py
+++ KitronikZip.py
@@ -1,204 +1,174 @@
 # microbit-module: KitronikZip@1.0.0
-# A module for the Kitronik Zip LEDs
-# Copyright (c) Kitronik Ltd 2019. 
-#
-# The MIT License (MIT)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-from microbit import *
 from neopixel import NeoPixel
 import math
 
 class ZipColours:
-#Simple class to hold colours in human readable manner
-		Red = (255,0,0)
-		Orange = (255,165,0)
-		Yellow = (255,255,0)
-		Green = (0,255,0)
-		Blue = (0,0,255)
-		Indigo = (75,0,130)
-		Violet = (138,43,226)
-		Purple = (255,0,255)
-		White = (255,255,255)
-		Black = (0,0,0)
-
+#Class to hold human readable colours
+ Red = (255,0,0)
+ Orange = (255,165,0)
+ Yellow = (255,255,0)
+ Green = (0,255,0)
+ Blue = (0,0,255)
+ Indigo = (75,0,130)
+ Violet = (138,43,226)
+ Purple = (255,0,255)
+ White = (255,255,255)
+ Black = (0,0,0)
 
 class ZipLEDs(NeoPixel):
-    # Default brightness is 50%
-    brightnessPercent = 50 
-    
-    #Set all thepixels to a colour
-    def setColour(self,colourToSet):
-        for pixel_id in range(0,len(self)):
-            self[pixel_id] = colourToSet
+ brightPer = 50 #Default is 50%
+ #len(self)
+ #Set all Zip LEDs to a colour
+ def setColour(self,colourToSet,start=0,length=60):
+  for pixel_id in range(start,start+length):
+   self[pixel_id] = colourToSet
+  self._applyBrightness()
 
-    #Show the colour on all the ZipLeds
-    def showColour(self,colourToShow):
-        self.setColour(colourToShow)
-        self._applyBrightness()
-    #Show a colour on a single Zip LED    
-    def showPixel(self,pixel,colourToShow):
-        self.setPixelColour(pixel,colourToShow)
-        self._applyBrightness()
-        
-    #Set a Zip LED to a colour but dont show it
-    def setPixelColour(self,pixel,colourToSet):
-        self[pixel] = colourToSet
+ #Show the colour on all the Zip LEDs
+ def showColour(self,colourToShow):
+  self.setColour(colourToShow)
+  self._applyBrightness()
+  self.show()
 
-    #suboptimal code because NeoPixel not really a list, it just has list like tendencies
-    def rotate(self, rotateBy):
-        rotated = 0
-        if(rotateBy > 0): #shift up
-            while rotated < rotateBy:
-                pixel = self[len(self)-1]
-                for pixel_id in range(len(self)-1,0,-1):
-                    self[pixel_id] = self[pixel_id-1]
-                self[0] = pixel
-                rotated = rotated+1
-        else: #shift down
-            while rotated > rotateBy:
-                pixel = self[0]
-                for pixel_id in range(0,len(self)-1):
-                    self[pixel_id] = self[pixel_id+1]
-                self[len(self)-1] = pixel
-                rotated = rotated-1 #avoid an ABS calculation for all the saving that it gives...
+ #Show colour on a single Zip LED 
+ def showPixel(self,pixel,colourToShow):
+  self.setPixelColour(pixel,colourToShow)
+  self._applyBrightness()
+  self.show()
 
-    # Set the brightness percentage, changes brightnessPercent variable
-    # Calls _applyBrightness to make changes visible
-    def setBrightness(self, brightness):
-        self.brightnessPercent = brightness
-        if (self.brightnessPercent <= 0):
-            self.brightnessPercent = 1
-        elif (self.brightnessPercent > 100):
-            self.brightnessPercent = 100
+ #Set a Zip LED to a colour
+ def setPixelColour(self,pixel,colourToSet):
+  self[pixel] = colourToSet
+  self._applyBrightness()
 
-        self._applyBrightness()
-    
-    # Apply the brightness setting and immediately show the changes
-    def _applyBrightness(self):
+ #Suboptimal as NeoPixel not really a list (list like tendencies)
+ def rotate(self, rotateBy):
+  rotated = 0
+  if(rotateBy > 0): #shift up
+   while rotated < rotateBy:
+    pixel = self[len(self)-1]
+    for pixel_id in range(len(self)-1,0,-1):
+     self[pixel_id] = self[pixel_id-1]
+    self[0] = pixel
+    rotated = rotated+1
+  else: #shift down
+   while rotated > rotateBy:
+    pixel = self[0]
+    for pixel_id in range(0,len(self)-1):
+     self[pixel_id] = self[pixel_id+1]
+    self[len(self)-1] = pixel
+    rotated = rotated-1 #avoid an ABS calculation for all the saving that it gives...
 
-        for pixel_id in range(0,len(self)):
-            pixelData = self[pixel_id]
-            r = pixelData[0]
-            g = pixelData[1]
-            b = pixelData[2]
-            brMultiplier = 2.55*self.brightnessPercent # brightnessMultipler = (255/100)*brightness
-            if (r == 0) and (g == 0) and (b ==0):
-                rBrightness = 0
-                gBrightness = 0
-                bBrightness = 0
-            elif (r > g): # r > g
-                if (r > b): # r > b
-                    rbRatio = b/r
-                    rgRatio = g/r
-                    rBrightness = math.floor(brMultiplier*1)
-                    gBrightness = math.floor(brMultiplier*rgRatio)
-                    bBrightness = math.floor(brMultiplier*rbRatio)
-                else: # b > r
-                    brRatio = r/b
-                    bgRatio = g/b
-                    rBrightness = math.floor(brMultiplier*brRatio)
-                    gBrightness = math.floor(brMultiplier*bgRatio)
-                    bBrightness = math.floor(brMultiplier*1)
-            else: # g > r
-                if (g > b):
-                    gbRatio = b/g
-                    grRatio = r/g
-                    rBrightness = math.floor(brMultiplier*grRatio)
-                    gBrightness = math.floor(brMultiplier*1)
-                    bBrightness = math.floor(brMultiplier*gbRatio)
-                else: # b > g
-                    bgRatio = g/b
-                    brRatio = r/b
-                    rBrightness = math.floor(brMultiplier*brRatio)
-                    gBrightness = math.floor(brMultiplier*bgRatio)
-                    bBrightness = math.floor(brMultiplier*1)
-            self[pixel_id] = (rBrightness, gBrightness, bBrightness)
-        self.show()
-    
-    #not sure that range can be done sensibly
-    #it would involve making a list of stuff that you could substitue into the base class somehow...
-    #def range(self, fromPixel, howMany):
-    #TODO?
-    
-        
-    def zipWavelength(self, wavelength):
-        blueGreen = 4.6364
-        redGreen = 2.55
-        redVal = 0
-        greenVal = 0
-        blueVal = 0
-       #The LEDs we are using have centre wavelengths of 470nm(Blue) 525nm(Green) and 625nm (Red) 
-       #We blend these linearly to give the impression of the other wavelengths. 
-        if ((wavelength >= 470) and (wavelength < 525)):
-            #We are between Blue and Green so mix those
-            greenVal = math.floor((wavelength - 470) * blueGreen)
-            blueVal = 255 - greenVal 
-        elif ((wavelength >= 525) and (wavelength <= 625)):
-            #we are between Green and Red, so mix those
-            redVal = math.floor((wavelength - 525) * redGreen)
-            greenVal = 255 - redVal
-        return (redVal, greenVal, blueVal)
+ #Set brightness percentage, changes brightPer
+ #0 not allowed, causes zero divide error
+ def setBrightness(self, brightness):
+  self.brightPer = brightness
+  if (self.brightPer <= 0):
+   self.brightPer = 1
+  elif (self.brightPer > 100):
+   self.brightPer = 100
 
-    def hueToRGB (self, hue):
-        redVal =0
-        greenVal =0
-        blueVal =0            
-        hueStep = 2.125 
-        if ((hue >= 0) and (hue < 120)): #RedGreen section
-            greenVal = math.floor((hue) * hueStep)
-            redVal = 255 - greenVal 
-        elif ((hue >= 120) and (hue < 240)): #GreenBlueSection
-            blueVal = math.floor((hue-120) * hueStep)
-            greenVal = 255 - blueVal
-        elif ((hue >= 240) and (hue < 360)): #BlueRedSection
-            redVal = math.floor((hue-240) * hueStep)
-            blueVal = 255 - redVal
+ #Apply the brightness setting
+ def _applyBrightness(self):
+  for pixel_id in range(0,len(self)):
+   r = self[pixel_id][0]
+   g = self[pixel_id][1]
+   b = self[pixel_id][2]
+   brMul = 2.55*self.brightPer #brMul = (255/100)*brightPer
+   if (r == 0) and (g == 0) and (b ==0):
+    rBr, gBr, bBr = 0, 0, 0
+   elif (r > g): # r > g
+    if (r > b): # r > b
+     rRatio = r/r
+     gRatio = g/r
+     bRatio = b/r
+    else: # b > r
+     rRatio = r/b
+     gRatio = g/b
+     bRatio = b/b
+   else: # g > r
+    if (g > b):
+     rRatio = r/g
+     gRatio = g/g
+     bRatio = b/g
+    else: # b > g
+     rRatio = r/b
+     gRatio = g/b
+     bRatio = b/b
+   rBr = math.floor(brMul*rRatio)
+   gBr = math.floor(brMul*gRatio)
+   bBr = math.floor(brMul*bRatio)
+   self[pixel_id] = (rBr, gBr, bBr)
 
-        return (redVal, greenVal, blueVal)
+ def zipWavelength(self, wavelength):
+  blueGreen = 4.6364
+  redGreen = 2.55
+  redVal = 0
+  greenVal = 0
+  blueVal = 0
+ #Zip LEDs have centre wavelengths of 470nm(Blue), 525nm(Green) & 625nm(Red) 
+ #Blended linearly to give impression of other wavelengths 
+  if ((wavelength >= 470) and (wavelength < 525)):
+   #Between Blue and Green so mix those
+   greenVal = math.floor((wavelength - 470) * blueGreen)
+   blueVal = 255 - greenVal 
+  elif ((wavelength >= 525) and (wavelength <= 625)):
+   #Between Green and Red so mix those
+   redVal = math.floor((wavelength - 525) * redGreen)
+   greenVal = 255 - redVal
+  return (redVal, greenVal, blueVal)
 
-            
-    def showRainbow(self,startHue,endHue):
-        hueRange = math.fabs(endHue-startHue)
-        hueStep = hueRange/len(self)
-        if(startHue<endHue): #conventional rainbow
-            for pixel_id in range(0,len(self)):
-                self[pixel_id] = self.hueToRGB(startHue+(pixel_id * hueStep))
-        else:#draw the rainbow backwards
-            for pixel_id in range(0,len(self)):
-                self[pixel_id] = self.hueToRGB(endHue-(pixel_id * hueStep))
-        self._applyBrightness()
- 
-    #a bar graph of green(lowest value) to red (Range)
-    def showBarGraph(self, valueToDisplay, graphRange): 
-        if (graphRange <= 0): #-ve value doesnt make sense, so just turn on first LED as green
-            self.clear()
-            self.showPixel(0,ZipColours.Green)
-        else:
-            value = math.fabs(valueToDisplay) #only interested in magnitude
-            numberOfPixels = len(self)
-            visible = (value * numberOfPixels)//graphRange
-            for i in range(numberOfPixels):
-                if(i <= visible):
-                    greenVal =(i * 255)//(numberOfPixels-1)
-                    self.setPixelColour(i, (greenVal, 255 - greenVal, 0))
-                else:
-                    self.setPixelColour(i, (0,0,0)) #turn off pixels over the display value
-        self._applyBrightness()
+ def hueToRGB (self, hue):
+  redVal =0
+  greenVal =0
+  blueVal =0
+  hueStep = 2.125
+  if ((hue >= 0) and (hue < 120)): #RedGreen section
+   greenVal = math.floor((hue) * hueStep)
+   redVal = 255 - greenVal
+  elif ((hue >= 120) and (hue < 240)): #GreenBlueSection
+   blueVal = math.floor((hue-120) * hueStep)
+   greenVal = 255 - blueVal
+  elif ((hue >= 240) and (hue < 360)): #BlueRedSection
+   redVal = math.floor((hue-240) * hueStep)
+   blueVal = 255 - redVal
+
+  return (redVal, greenVal, blueVal)
+
+ def showRainbow(self,startHue,endHue):
+  hueRange = math.fabs(endHue-startHue)
+  hueStep = hueRange/len(self)
+  if(startHue<endHue): #conventional rainbow
+   for pixel_id in range(0,len(self)):
+    self[pixel_id] = self.hueToRGB(startHue+(pixel_id * hueStep))
+  else:#draw rainbow backwards
+   for pixel_id in range(0,len(self)):
+    self[pixel_id] = self.hueToRGB(endHue-(pixel_id * hueStep))
+  self._applyBrightness()
+  self.show()
+
+ #Bar graph of green(lowest value) to red (Range)
+ def showBarGraph(self, valueToDisplay, graphRange): 
+  if (graphRange <= 0): #-ve value doesnt make sense, so just turn on first LED as green
+   self.clear()
+   self.showPixel(0,ZipColours.Green)
+  else:
+   value = math.fabs(valueToDisplay) #only interested in magnitude
+   numberOfPixels = len(self)
+   visible = (value * numberOfPixels)//graphRange
+   for i in range(numberOfPixels):
+    if(i <= visible):
+     greenVal =(i * 255)//(numberOfPixels-1)
+     self.setPixelColour(i, (greenVal, 255 - greenVal, 0))
+    else:
+     self.setPixelColour(i, (0,0,0)) #turn off pixels over the display value
+  self._applyBrightness()
+  self.show()
+
+class RangeLEDs():
+ def __init__(self, startPixel, length):
+  self.start = startPixel
+  self.len = length
+
+ def setColour(self,zipLeds,colourToSet):
+  zipLeds.setColour(colourToSet,self.start,self.len)
